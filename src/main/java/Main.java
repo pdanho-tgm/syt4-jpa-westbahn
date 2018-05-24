3d9216f4ea10c8928fe46f7fb5b85143eceb70d0
Patrick <pdanho@student.tgm.ac.at>
2018-05-24 16:26:22 +0200

    getter setter für die model-klassen

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,6 @@
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.List;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
@@ -7,59 +8,52 @@ import javax.persistence.Persistence;
 import javax.persistence.PersistenceContext;
 
 import org.apache.log4j.Level;
+import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
 
 public class Main {
-
-	private static final Logger log = Logger.getLogger(Main.class);
-	private static EntityManagerFactory sessionFactory;
-	@PersistenceContext
-	private static EntityManager entitymanager;
-	static SimpleDateFormat dateForm = new SimpleDateFormat("dd.MM.yyyy");
-	static SimpleDateFormat timeForm = new SimpleDateFormat("dd.MM.yyyy mm:hh");
-
-	private Main() {
-		super();
-	}
+	private static final Logger logger = LogManager.getLogger(Main.class.getName());
+	private static long BAHNHOF_ID = 1L;
 
 	public static void main(String[] args) {
-		log.setLevel(Level.ALL);
-		try {
-			log.info("Starting \"Mapping Perstistent Classes and Associations\" (task1)");
-			sessionFactory = Persistence.createEntityManagerFactory("westbahn");
-			
-			fillDB(entitymanager);
-			task01();
-			log.info("Starting \"Working with JPA-QL and the Hibernate Criteria API\" (task2)");
-			task02a();
-			task02b();
-			task02c();
-		} catch (ParseException e) {
-			e.printStackTrace();
-		} catch (Exception e) {
-			e.printStackTrace();
+		logger.info("Starting EntityManagerFactory..");
+
+		ValidatorFactory validatorFactory = Validation.buildDefaultValidatorFactory();
+		Validator validator = validatorFactory.getValidator();
+
+		EntityManagerFactory factory = Persistence.createEntityManagerFactory("westbahnPU");
+		EntityManager entityManager = factory.createEntityManager();
+
+		createStations(entityManager);
+		createStrecken(entityManager);
+		createUsers(entityManager);
+		createSonderangebote(entityManager);
+		//update(entityManager);
+		//remove(entityManager);
+
+		List<Benutzer> benutzerMitMonatskarte = entityManager.createNamedQuery("Benutzer.getAllWithMonatskarte").getResultList();
+		for (Benutzer benutzer : benutzerMitMonatskarte) {
+			System.out.println("Benutzer mit Monatkarte: " + benutzer);
 		}
-	}
-	
-	public static void fillDB(EntityManager em) throws ParseException {
-		em = sessionFactory.createEntityManager();
-		
-	}
-	
-	public static void task01() throws ParseException, InterruptedException {
-		
-		
-	}
-	public static void task02a() throws ParseException {
 
-	}
+		List<Reservierung> benutzerMitReservierungen = entityManager.createNamedQuery("Reservierung.getReservierungenForUser")
+				.setParameter("email", email)
+				.getResultList();
+		for (Reservierung reservierung : benutzerMitReservierungen) {
+			System.out.println("Reservierung für " + email + ": " + reservierung);
+		}
 
-	public static void task02b() throws ParseException {
+		long streckeId = 1;
+		List<Ticket> ticketsOhneReservierung = entityManager.createNamedQuery("Ticket.getAllTicketsWithoutReservierung")
+				.setParameter("streckeID", streckeId)
+				.getResultList();
+		for (Ticket ticket : ticketsOhneReservierung) {
+			System.out.println("Strecke ohne Reservierung für Strecke#" + streckeId + ": " + ticket);
+		}
 
-	}
-
-	public static void task02c() throws ParseException {
-
+		entityManager.close();
+		factory.close();
+		logger.info("Finished!");
 	}
 
 }
